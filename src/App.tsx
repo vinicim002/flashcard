4fabece2661242caee7e4eb8c7b695cce8314ca4
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-01-08 15:11:55 -0300

    Esqueleto do html finalizado => Iniciando css da pagina flashcard

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./styles/global.css";
+import "./styles/theme.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
-    <>
-      <div className="bg-amber-400">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app w-screen h-screen">
+      <nav className="flex">
+        <div>LOGO</div>
+        <button type="button">HOME</button>
+      </nav>
+
+      <div className="mainContainer flex justify-center gap-3">
+        <div className="containerFlashcard bg-bg-flashcard border-4 border-primary-flashcard rounded-2xl p-5">
+          {/*Categorias do deck*/}
+          <div className="optionsFlashCard">
+            {/*Dropdown de categorias*/}
+            <button type="button" className="border-white-flashcard">
+              Categorias
+            </button>
+            {/*Checkbox para esconder cards masterizados*/}
+            <div>
+              <input type="checkbox" />
+              <label htmlFor="">Esconder masterizadas</label>
+            </div>
+          </div>
+          {/*Card*/}
+          <div className="flascard">
+            <div className="frontCard">
+              {/*Categora*/}
+              <div className="categoriaFlashCard">Hooks</div>
+              <h2>O que é um hook?</h2>
+              <p>Click para revelar a resposta</p>
+              <div>
+                <div>barra</div>
+                <div>1/10</div>
+              </div>
+            </div>
+          </div>
+          {/*Buttons de resete e saber card*/}
+          <div className="buttonsFlashCard">
+            <button>Eu sei essa</button>
+            <button>Resetart Progresso</button>
+          </div>
+          <div className="buttonsVoltarProximo">
+            <button>Voltar</button>
+            <p>Card 1 de 40</p>
+            <button>Próximo</button>
+          </div>
+        </div>
+        {/*Segundo container*/}
+        <div className="containerEstatisticaCards bg-amber-300">
+          {/*Estatísticas dos cards*/}
+          <div className="estatisticaCards">
+            <h2>Estatísticas</h2>
+            {/*Cards de estatisticas*/}
+            <div>
+              <div>
+                <h3>Total de cards</h3>
+                <h3>40</h3>
+              </div>
+              <p>icon</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
-
-export default App;
